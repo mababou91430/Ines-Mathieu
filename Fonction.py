8bf22c323df519dca0ae3c6794749896bd758f04
mababou91430 <math-91@orange.fr>
2023-11-24 09:20:04 +0100

    Création du fichier fonction

--- Fonction.py
+++ Fonction.py
@@ -2,7 +2,18 @@ import os
 
 
 def nom_president():
+    L = []
+    remplacement = "'!?123456789"
     for filename in os.listdir("speeches-20231124"):
-        print(filename)
+        for i in range(len(remplacement)):
+            filename = filename.replace(remplacement[i],     "")
+        filename = filename.replace("Nomination_", "")
+        filename = filename.replace(".txt", "")
+        if filename not in L:
+            L.append(filename)
+    return L
 
-nom_president()
+
+
+nom = nom_president()
+print(nom)
