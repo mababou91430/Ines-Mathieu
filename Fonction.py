f5fe3b3ce220e8a14e29e614de7767e7010efc25
mababou91430 <math-91@orange.fr>
2023-11-24 13:01:12 +0100

    Création du fichier fonction

--- Fonction.py
+++ Fonction.py
@@ -18,14 +18,18 @@ def clean():
     for filename in os.listdir("speeches-20231124"):
         mon_fichier = "speeches-20231124/" + filename
         f1 = open(mon_fichier, "r")
+        text = "cleaned/" + filename
+        f = open(text, "w")
         ligne = f1.readline()
         while ligne != "":
+            l = list(ligne)
             for i in range(len(ligne)):
-                if 65 <= ord(ligne[i]) <= 90:
-                    l = list(ligne)
+                if 65 <= ord(l[i]) <= 90:
                     l[i] = chr(ord(l[i]) + 32)
+            ligne = "".join(l)
+            f.write(ligne)
             ligne = f1.readline()
-        text = "cleaned/" + filename
+
 
 
 clean()
