227d782eb3f7b7fed0110fb332b09a83dc6d5622
mababou91430 <math-91@orange.fr>
2023-11-29 18:25:55 +0100

    Création du fichier fonction

--- IDF.py
+++ IDF.py
@@ -3,13 +3,24 @@ import os
 
 
 def IDF():
-    L = []
+    Dictionnaire = {}
     for filename in os.listdir("cleaned"):
         text = "cleaned/" + filename
         f1 = open(text, "r")
         ligne = f1.readline()
+        temp = ""
+        p = True
         while ligne != "":
             L1 = ligne.split(" ")
+            for i in range(len(L1)):
+                for j in Dictionnaire.keys():
+                    if j == L1[i]:
+                        p = False
+                        temp = L1[i]
+            if p == True:
+
+
+
             ligne = f1.readline()
             print(L1)
 
