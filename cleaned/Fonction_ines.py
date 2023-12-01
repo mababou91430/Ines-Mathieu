fafa7f424ad7fbad4b280c3cbade29e6449963f0
mababou91430 <math-91@orange.fr>
2023-12-01 09:45:58 +0100

    Création du fichier fonction

---
+++ cleaned/Fonction_ines.py
@@ -0,0 +1,91 @@
+import os
+
+
+def nom_president():
+    L = []
+    remplacement = "'!?123456789"
+    for filename in os.listdir("speeches-20231124"):
+        for i in range(len(remplacement)):
+            filename = filename.replace(remplacement[i], "")
+        filename = filename.replace("Nomination_", "")
+        filename = filename.replace(".txt", "")
+        if filename not in L:
+            if filename == "Hollande":
+                filename = " François" +" "+ filename
+                L.append(filename)
+
+            if filename == "Chirac":
+                filename = " Jacques" +" "+ filename
+                L.append(filename)
+
+            if filename == "Sarkozy":
+                filename = " Nicolas" +" "+ filename
+                L.append(filename)
+
+            if filename == "Giscard dEstaing":
+                filename = " Valéry" +" "+ filename
+                L.append(filename)
+
+            if filename == "Mitterant":
+                filename = " François" +" "+ filename
+                L.append(filename)
+
+            if filename == "Macron":
+                filename = " Emmanuel" +" "+ filename
+                L.append(filename)
+    return L
+
+
+#print(nom_president())
+
+def clean():
+    for filename in os.listdir("speeches-20231124"):
+        mon_fichier = "speeches-20231124/" + filename
+        f1 = open(mon_fichier, "r")
+        repertoire_nom_fichier = "cleaned/" + filename
+        f = open(repertoire_nom_fichier, "w")
+        ligne = f1.readline()
+        while ligne != "":
+            l = list(ligne)
+            for i in range(len(ligne)):
+                if 65 <= ord(l[i]) <= 90:
+                    l[i] = chr(ord(l[i]) + 32)
+                if (33 <= ord(l[i]) <= 47) or (58 <= ord(l[i]) <= 64) or (91 <= ord(l[i]) <= 96) or (123 <= ord(l[i]) <= 126 ) :
+                    if ord(l[i])== 45 or 44:
+                        l[i] = chr(32)
+                    else:
+                        l[i]= chr(0)
+            ligne = "".join(l)
+            f.write(ligne)
+            ligne = f1.readline()
+
+
+
+#clean()
+
+
+def TF(name):
+    dic = {}
+    a = 0
+    b = 0
+    f1 = open(name, "r")
+    ligne = f1.readline()
+    p = True
+    while ligne != "":
+        L1 = ligne.split(" ")
+        for i in range(len(L1)):
+            if L1[i] in dic:
+                dic[L1[i]] = dic[L1[i]] + 1
+            else:
+                for j in range(i, len(L1)):
+                    if L1[j] == L1[i]:
+                        a += 1
+                dic[L1[i]] = a
+                a = 0
+        ligne = f1.readline()
+
+    print(dic)
+    return(dic)
+
+
+TF("cleaned/Nomination_Chirac1.txt")
